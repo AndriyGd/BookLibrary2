edb819627a553d41a937d78c8e8176dde8e3c2ee
kspde <kspde@ALGRABSYS>
2016-09-07 15:32:39 +0300

    Test class Library

--- SimpleBookLibrary/SimpleBookLibrary/Program.cs
+++ SimpleBookLibrary/SimpleBookLibrary/Program.cs
@@ -10,6 +10,24 @@ namespace SimpleBookLibrary
     {
         static void Main(string[] args)
         {
+            Library lib = new Library();
+
+            Book b1 = new Book
+            {
+                Name = "Lacy Starr",
+                Author = new Author { Age = 54, Name = "Isaac Asimow"},
+                DatePublish = new DateTime(1956, 9, 23),
+                Version = 2
+            };
+
+            lib.AddBook(b1);
+
+            for (int i = 0; i < lib.CountBook; i++)
+            {
+                Console.WriteLine(lib[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
